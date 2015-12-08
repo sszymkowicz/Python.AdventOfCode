3ce6561eddb49d9d4c3e620fe921c1f6479c223b
Sławek <slawus420@gmail.com>
2015-12-08 15:21:42 +0100

    Day 4

--- day4.py
+++ day4.py
@@ -6,4 +6,4 @@ i = 0
 while hashlib.md5((in_var+str(i)).encode()).hexdigest()[0:zeros] != "0"*zeros:
     i += 1
 print("MD5 hash: " + hashlib.md5((in_var+str(i)).encode()).hexdigest())
-print("Answer: " + i)
+print("Answer: " + str(i))
